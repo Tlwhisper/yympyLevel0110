a20d279a048788897615c6efd0ce6e5ef46077e5
yangyanmeng <583692365@qq.com>
2021-05-29 19:26:20 +0800

    add 01.py

---
+++ 01.py
@@ -0,0 +1,9 @@
+print('----I love work----')
+temp = input("guss what I am want?")
+guess = int(temp)
+if guess == 8:
+    print("good you are right!!!")
+    print("but not fish")
+else:
+    print("wrong answer")
+print("game over")
