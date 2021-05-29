03d030c1fcfebee30e194b7b34b2dbd9b49da69e
yangyanmeng <583692365@qq.com>
2021-05-29 20:37:22 +0800

    给出提示大小的信息

--- 01_1.py
+++ 01_1.py
@@ -1,10 +1,13 @@
 print('----I love work----')
-temp = input("guss what I am want?")
+temp = input("guss what I am want?\n")
 guess = int(temp)
 if guess == 8:
     print("good you are right!!!")
     print("but not fish")
 else:
-    print("wrong answer")
+    if guess > 8:
+        print("too big")
+    else:
+        print("too small")
 print("game over")
 
