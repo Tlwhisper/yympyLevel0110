6bf81101beeaa58d1eaeeb888ba42c9f0e4343cb
yangyanmeng <583692365@qq.com>
2021-05-30 18:02:47 +0800

    for语句,range_BIF,continue,break,

---
+++ 99_1.py
@@ -0,0 +1,26 @@
+text = "yangyanmeng"
+for i in text:
+    print(i + ' ')
+
+name = ["jack","tianxia","huangzi","demaxiya"]
+for it in name:
+    print(it,len(it))
+
+for i in range(5):
+    print(i)
+
+print("next line\n")
+for i in range(2,8):
+    print(i)
+
+print("next line\n")
+for i in range(1,10,2):
+    print(i)
+
+print("next line\n")
+for i in range(10):
+    if i%2 != 0:
+        print(i)
+        continue
+    i += 5
+    print(i)
