8d2791051c4b0660ba01df7c84ef73adeec36973
yym <583692365@qq.com>
2021-11-04 21:14:50 +0800

    增加一个type函数，和int()、str()、float()强制转换类型

--- 02_1_fun.py
+++ 02_1_fun.py
@@ -1,2 +1,16 @@
+# 测试type函数
 a = "haha"
-print(type(a))
+
+# 强制转换类型
+a = '1234'
+b = int(a)
+c = float(a)
+d = str( b)
+print("a 的数据类型" )
+print( type(a))
+print("b 的数据类型") 
+print( type(b))
+print("c的数据类型") 
+print( type(c))
+print("d 的数据类型")
+print( type(d))
